5ad1e6257a259f80d8ada220f340f5a63c30b69f
arita37 <noelkev0@gmail.com>
2021-07-01 21:21:59 +0900

    code_parser.py

---
+++ code_parser.py
@@ -0,0 +1,87 @@
+
+
+
+
+
+#####################################################################
+def get_list_function_name(file):
+
+    if line.startwith("def "):
+    	fname = line.split("(")[0]
+    	fname = fname.replace("def ", "")
+        fname_list.append(fname)
+
+	return fname_list
+
+
+
+def get_list_class_name(file):
+
+
+	return fname_list
+
+
+
+def get_list_class_methods(file):
+
+
+	return fname_list
+
+
+
+def get_list_variable_global(file):
+
+
+	return  llist
+
+
+
+
+def get_list_function_stats(file):
+    """
+      nb of lines per function
+      nb of variables   ==  string word
+      
+
+
+    """
+
+	return stats
+
+
+
+
+def get_file_stats(file):
+    """
+      nb of functions per file
+      nb of classes   per file
+      Avg lines per function  into the file
+
+    """
+
+	return stats
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
